dfbacdc6c5d48be0f4190c02c538c84ed0704b76
bantu1410 <naveen.bantu1@gmail.com>
2020-10-10 11:15:35 +0200

    appending all csv files together - with header set to None while reading the CSV files

--- combine2.py
+++ combine2.py
@@ -9,13 +9,13 @@ all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
 print(all_filenames)
 
-read_csv = []
-
+#read_csv = []
 #combine all files in the list
-for f in all_filenames:
-    read_csv.append(pd.read_csv(f, sep=',', encoding='utf-8'))
-print(read_csv)
-#combined_csv = pd.concat([pd.read_csv(f, sep=',', encoding='utf-8') for f in all_filenames])
+#for f in all_filenames:
+ #   read_csv.append(pd.read_csv(f, sep=',', encoding='utf-8'))
+#print(read_csv)
 
-combined_csv = pd.concat(read_csv)
-print(combined_csv)
+combined_csv = pd.concat([pd.read_csv(f, header=None) for f in all_filenames], ignore_index=True)
+
+#combined_csv = pd.concat(read_csv)
+combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
