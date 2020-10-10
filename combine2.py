1ff619813c2e0e0f022876964d2d49ab3a5a791a
bantu1410 <naveen.bantu1@gmail.com>
2020-10-10 11:54:19 +0200

    Specific header defined for the combined csvs code cleaning

--- combine2.py
+++ combine2.py
@@ -2,20 +2,16 @@ import os
 import pandas as pd
 import glob
 
-os.chdir("F:\\python_test\\")
+os.chdir('D:\\Python_DEV\\RunLogic APP\\')
 
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
 print(all_filenames)
 
-#read_csv = []
 #combine all files in the list
-#for f in all_filenames:
- #   read_csv.append(pd.read_csv(f, sep=',', encoding='utf-8'))
-#print(read_csv)
+header_names = ['Timestamp', ' CmpId', 'ClassId', 'ErrorId', 'InfoId', 'InfoText']
+combined_csv = pd.concat([pd.read_csv(f, header=None, comment=';', names=header_names) for f in all_filenames], ignore_index=True)
 
-combined_csv = pd.concat([pd.read_csv(f, header=None) for f in all_filenames], ignore_index=True)
-
-#combined_csv = pd.concat(read_csv)
+print(combined_csv.info())
 combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
